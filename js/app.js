ad6913cfb02a9c78391508453a6f38c47370edc1
jrdurrcodes <biycoachn@yahoo.com>
2025-02-13 10:34:28 -0600

    constructor built for tvshows

---
+++ js/app.js
@@ -0,0 +1,82 @@
+/**build array 12 tv shows 
+ * {
+ *    id,
+ *    title,
+ *    genre[],
+ *    yearStart
+ *    yearEnd,
+ *    seasonCount,
+ *    episodeCount,
+ *    network => disney, nickelodeon, pbs, cartoon network,
+ *    creator
+ *    isFavorite
+ * }
+ * 
+ * build cards
+ * 
+ * load cards
+ * 
+ * filter
+ * 
+*/
+
+class Shows {
+    constuctor() {
+        this.data = [
+        { 
+            id: 'cn001',
+            title: 'adventure time',
+            genre: ['comedy', 'adventure', 'fantasy', 'action'],
+            yearStart: new Date('April 5, 2010'),
+            yearEnd: new Date(),
+            seasonCount: ,
+            episodeCount: 283,
+            network: 'cartoon network',
+            creator: 'pendleton ward',
+            isFavorite: false
+        },
+        {
+            id: '',
+            title: '',
+            genre: [''],
+            yearStart: new Date(''),
+            yearEnd: new Date,
+            seasonCount: new Date('  '),
+            episodeCount: ,
+            network: '',
+            creator: 'krat',
+            isFavorite: false
+        },
+        {
+            id: 'pbs002'
+            title: 'arthur',
+            genre: ['comedy', 'fantasy', 'family'],
+            yearStart: new Date('April 5, 2010')  
+            yearEnd: new Date
+            seasonCount: new Date('September  ')
+            episodeCount: 283
+            network: 'cartoon network',
+            creator: 'pendleton ward',
+            isFavorite: false
+        },
+        {
+            id: '',
+            title: '',
+            genre: [''],
+            yearStart: new Date('')  
+            yearEnd: new Date
+            seasonCount: new Date(' ')
+            episodeCount: 
+            network: '',
+            creator: '',
+            isFavorite: false
+        },
+{
+
+},
+
+           }
+        ]
+    }
+}
+
